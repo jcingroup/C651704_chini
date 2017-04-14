a7bf512085db8680a6cf388f7655ca376b7c5c94
TinWu <yyuuix@gmail.com>
2017-04-14 14:25:45 +0800

    add proj

---
+++ Work.WebProj/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using System.Web.Mvc;
+
+namespace Work.WebProj.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult AboutUs()
+        {
+            return View();
+        }
+
+        public ActionResult ContactUs()
+        {
+            return View();
+        }
+    }
+}
